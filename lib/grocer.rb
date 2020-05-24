298c9abd101e8de6cf31cea0ae3f7d683b2bfba5
Scott Espinosa <scottjames.espinosa@gmail.com>
2020-05-24 21:46:53 +0000

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,17 +1,27 @@
-def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+require 'pry'
 
-end
+def find_item_by_name_in_collection(item, shelf)
+  returned_item = nil
+  shelf.each do |item_hash|
+    item_hash.each do |item_name, item_details|
+      if item_details == item 
+        returned_item = item_hash
+      end 
+    end 
+  end 
+  returned_item
+end 
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
+  new_cart = []
+  cart.each do |item_hash|
+    main_item = find_item_by_name_in_collection(item_hash[:item], new_cart)
+    binding.pry
+      if main_item 
+        main_item[:count] += 1 
+      else 
+        item_hash[:count] = 1 
+        new_cart << item_hash
+      end 
+  end 
 end
-
-
-  
--- random_grocery_generator.rb
+++ random_grocery_generator.rb
@@ -1,4 +1,5 @@
 require_relative 'grocer'
+require 'pry'
 
 def items
 	[
