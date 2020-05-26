934e627ad768176bc5e9da48cd118144d78f79d4
Scott Espinosa <scottjames.espinosa@gmail.com>
2020-05-26 03:18:52 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -14,14 +14,21 @@ end
 
 def consolidate_cart(cart)
   new_cart = []
-  cart.each do |item_hash|
-    main_item = find_item_by_name_in_collection(item_hash[:item], new_cart)
+  cart.each do |cart_item|
+    new_cart_item = find_item_by_name_in_collection(cart_item[:item], new_cart)
     binding.pry
-      if main_item 
-        main_item[:count] += 1 
+      if new_cart_item != nil 
+        new_cart_item[:count] += 1 
       else 
-        item_hash[:count] = 1 
-        new_cart << item_hash
+        new_cart_item = {
+          :item => cart_item[:item],
+          :price => cart_item[:price],
+          :clearance => cart_item[:clearance],
+          :count => 1 
+        }
+        binding.pry
+        new_cart << new_cart_item
       end 
   end 
+  new_cart
 end
